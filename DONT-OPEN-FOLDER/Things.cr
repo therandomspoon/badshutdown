4cace687125f0f4e1cb4060f89d3edf2cf05da1f
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2022-12-09 10:13:40 +0000

    Create Things.cr

---
+++ DONT-OPEN-FOLDER/Things.cr
@@ -0,0 +1,19 @@
+$ crystal
+Usage: crystal [command] [switches] [program file] [--] [arguments]
+
+Command:
+    init                     generate a new project
+    build                    build an executable
+    docs                     generate documentation
+    env                      print Crystal environment information
+    eval                     eval code from args or standard input
+    play                     starts Crystal playground server
+    run (default)            build and run program
+    spec                     build and run specs (in spec directory)
+    tool                     run a tool
+    help, --help, -h         show this help
+    version, --version, -v   show version
+
+Run a command followed by --help to see command-specific information, ex:
+    crystal <command> --help
+puts "Hello World!"
